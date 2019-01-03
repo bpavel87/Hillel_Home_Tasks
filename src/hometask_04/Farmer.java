abdfafdfe4a9c2bcf290ce20ba6942506e239ce8
Pavel Boyko <octopusmotion87@gmail.com>
2019-01-03 18:56:39 +0200

    first

---
+++ src/hometask_04/Farmer.java
@@ -0,0 +1,11 @@
+package hometask_04;
+
+public class Farmer {
+    public static void main(String[] args) {
+        int lenght = 10;
+        int with = 20;
+        int perimetr = 2 * (lenght + with);
+        int square = lenght * with;
+        System.out.println(perimetr);
+    }
+}
---
+++ src/hometask_04/HryvnaDollar.java
@@ -0,0 +1,10 @@
+package hometask_04;
+
+public class HryvnaDollar {
+
+    public static void main(String[] args) {
+        float dollar = 300;
+        float hryvna = dollar / 27;
+        System.out.println(hryvna);
+    }
+}
